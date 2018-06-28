9362b22c0149868b973bc659d464e59327d99f74
kevin0525 <759424614@qq.com>
2018-06-28 21:46:54 +0800

    create a project
    create a project

---
+++ svm_hog/svm_hog/main_run.cpp
@@ -0,0 +1,140 @@
+//#include <iostream>  
+//#include <fstream>  
+//#include <strstream>
+//#include <opencv2/core/core.hpp>  
+//#include <opencv2/highgui/highgui.hpp>  
+//#include <opencv2/imgproc/imgproc.hpp>  
+//#include <opencv2/objdetect/objdetect.hpp>  
+//#include <opencv2/ml/ml.hpp>  
+//#include "someMethod.h"
+//#include "parameter.h"
+//using namespace std;  
+//using namespace cv;  
+//
+//int main()  
+//{
+//	//svm,hog
+//    MySVM detectSvm;
+//	MySVM classifySvm;
+//	detectSvm.load(DetectSvmName);
+//	classifySvm.load(ClassifySvmName);
+//	HOGDescriptor detectHOG(WinSizeDetect,BlockSizeDetect,BlockStrideDetect,CellSizeDetect,NbinsDetect,1,-1,0,0.2,false,10);
+//	HOGDescriptor classifyHOG(WinSizeClassify,BlockSizeClassify,BlockStrideClassify,CellSizeClassify,NbinsClassify);//for calculating HOG
+//	setHOG(detectSvm, detectHOG);//set detectHOG from detectSvm
+//	int descriptorDimClassify = classifySvm.get_var_count();
+//	//result files
+//	const char * sd1 = {"md "ResultVideoFile_1};//for saving result image
+//	system(sd1);
+//	const char * sd2 = {"md "ResultVideoFile_2};
+//	system(sd2);
+//	const char * sd3 = {"md "ResultVideoFile_3};
+//	system(sd3);
+//	//video
+//	VideoCapture myVideo(TestVideo);
+//	if(!myVideo.isOpened()){cout<<"read video error"<<endl;system("puase");return -1;}
+//	double videoRate=myVideo.get(CV_CAP_PROP_FPS);
+//	int videoWidth=myVideo.get(CV_CAP_PROP_FRAME_WIDTH);
+//	int videoHight=myVideo.get(CV_CAP_PROP_FRAME_HEIGHT);
+//	int videoDelay=1000/videoRate;
+//	VideoWriter outputVideo(ResultVideo, CV_FOURCC('M', 'J', 'P', 'G'), videoRate, Size(videoWidth, videoHight));
+//	//frame
+//	Mat src,dst;
+//	//label
+//	LabelRobot label_irobot(50,10);
+//	LabelRobot label_obstacle(50,4);
+//	//begin
+//	bool stop = false;
+//	for (int fnum = 1;!stop;fnum++)
+//	{
+//		cout<<"frame: "<<fnum<<endl;
+//		if (!myVideo.read(src)){cout<<"video end"<<endl;waitKey(0); break;}//get a frame
+//		//resize(videoFrame,videoFrame,Size(0,0),2,2);
+//		src.copyTo(dst);
+//		vector<Rect> found;//detect result
+//		cout<<"detect"<<endl;
+//		detectHOG.detectMultiScale(src, found, HitThreshold, WinStride, Size(0,0), DetScale, 0.2, true);
+//			//parameter: 1.src image 2.detect location result 3.(optional)detect weight result 4.offset of hyperplane 
+//			//5. window stride (must be integer multiple to block stride!!!) 6.extend on src edge 7.scale resize ratio for every detection 
+//			//8.cluster parameter (if meanshift cluster flag is false, parameter means minimum possible number of rectangles minus 1. if true, parameter's function is similar to HitThreshold)
+//			//9.use meanshift cluster flag(false-cluster, still have overlap; true-meanshift cluster, without overlap)
+//		found = resize_boxes(found, src, DetectResizeRate);
+//
+//		//label
+//		vector<RobotMessage> irobots_message;
+//		vector<RobotMessage> obstacles_message;
+//		//classify
+//		for(int i=0; i<found.size(); i++)  
+//		{
+//			cout<<"width:"<<found[i].width<<"  height:"<<found[i].height<<endl;//detect windows
+//			vector<float> descriptors;//classily HOG descriptor
+//			Mat descriptorsMat = Mat::zeros(1, descriptorDimClassify, CV_32FC1);
+//			Mat temp;
+//			resize(src(found[i]),temp,WinSizeClassify);//resize detect result image for classify
+//			classifyHOG.compute(temp,descriptors);//calculate classily HOG descriptor
+//			for(int i=0; i<descriptorDimClassify; i++)  
+//				descriptorsMat.at<float>(0,i) = descriptors[i];
+//			float classifyResult = classifySvm.predict(descriptorsMat);//classily result
+//			if (classifyResult == 1)//irobot
+//			{
+//				irobots_message.insert(irobots_message.end(),RobotMessage(found[i]));
+//				rectangle(dst, found[i], Scalar(255,0,0), 3);
+//				if (SAVESET)//save classify result
+//				{
+//					strstream ss;
+//					string s;
+//					ss<<ResultVideoFile_1<<1000*fnum+i<<".jpg";
+//					ss>>s;
+//					imwrite(s,src(found[i]));
+//				}
+//			} 
+//			else if (classifyResult == 2)//obstacle
+//			{
+//				obstacles_message.insert(obstacles_message.end(),RobotMessage(found[i]));
+//				rectangle(dst, found[i], Scalar(0,255,0), 3);
+//				if (SAVESET)//save classify result
+//				{
+//					strstream ss;
+//					string s;
+//					ss<<ResultVideoFile_2<<1000*fnum+i<<".jpg";
+//					ss>>s;
+//					imwrite(s,src(found[i]));
+//				}
+//			}
+//			else if (classifyResult ==3)//background
+//			{
+//				rectangle(dst, found[i], Scalar(0,0,255), 3);
+//				if (SAVESET)//save classify result
+//				{
+//					strstream ss;
+//					string s;
+//					ss<<ResultVideoFile_3<<1000*fnum+i<<".jpg";
+//					ss>>s;
+//					imwrite(s,src(found[i]));
+//				}
+//			}
+//		}
+//		//label
+//		label_irobot.input(irobots_message);
+//		label_irobot.getLabel(irobots_message);
+//		label_obstacle.input(obstacles_message);
+//		label_obstacle.getLabel(obstacles_message);
+//		for (int i = 0; i < irobots_message.size(); i++)
+//		{
+//			char label[8];
+//			sprintf(label,"%d",irobots_message[i].label);
+//			putText(dst,label,irobots_message[i].center,FONT_HERSHEY_SIMPLEX,2,CV_RGB(0,0,0),2,10);
+//			//put word on image: image,word,pos,font,scale,color,thickness,line type
+//		}
+//		for (int i = 0; i < obstacles_message.size(); i++)
+//		{
+//			char label[8];
+//			sprintf(label,"%d",obstacles_message[i].label);
+//			putText(dst,label,obstacles_message[i].center,FONT_HERSHEY_SIMPLEX,2,CV_RGB(0,0,0),2,10);
+//			//put word on image: image,word,pos,font,scale,color,thickness,line type
+//		}
+//		//save frame to video
+//		outputVideo<<dst;
+//		imshow("dst",dst);
+//		if(waitKey(1)>=0)stop = true;
+//	}
+//}
---
+++ svm_hog/svm_hog/parameter.h
@@ -0,0 +1,100 @@
+//-----------------------宏定义----------------------------
+//---------------------------------------------------------
+#define IrobotSetNo 430			//机器人样本个数  
+#define ObstacleSetNo 239		//障碍样本个数
+#define BackgroundSetNo 412		//背景样本个数
+#define HardBackgroundSetNo 253	//Hard背景样本个数
+
+#define SHOWSET false			//是否显示训练样本
+#define TRAIN true				//是否进行训练,true表示训练，false表示读取xml文件中的SVM模型
+#define SAVESET true			//是否保存检测数据
+
+/*//for cpu
+//HOG描述子参数
+#define WinSizeDetect Size(32,16)		//检测窗口尺寸
+#define BlockSizeDetect Size(8,8)		//block尺寸
+#define BlockStrideDetect Size(4,4)		//block步长
+#define CellSizeDetect Size(4,4)		//cell尺寸
+#define NbinsDetect 9					//直方图bin个数
+
+//HOG描述子参数
+#define WinSizeClassify Size(48,24)		//检测窗口尺寸
+#define BlockSizeClassify Size(16,8)		//block尺寸
+#define BlockStrideClassify Size(4,4)	//block步长
+#define CellSizeClassify Size(4,4)		//cell尺寸
+#define NbinsClassify 9					//直方图bin个数
+*/
+
+///////////for GPU
+//HOG描述子参数
+#define WinSizeDetect Size(40,40)		//检测窗口尺寸
+#define BlockSizeDetect Size(16,16)		//block尺寸
+#define BlockStrideDetect Size(8,8)		//block步长
+#define CellSizeDetect Size(8,8)		//cell尺寸
+#define NbinsDetect 9					//直方图bin个数
+
+//HOG描述子参数
+#define WinSizeClassify Size(40,40)		//检测窗口尺寸
+#define BlockSizeClassify Size(16,16)		//block尺寸
+#define BlockStrideClassify Size(8,8)	//block步长
+#define CellSizeClassify Size(8,8)		//cell尺寸
+#define NbinsClassify 9					//直方图bin个数
+
+////HOG描述子参数
+//#define WinSize Size(20,10)		//检测窗口尺寸
+//#define BlockSize Size(4,4)		//block尺寸
+//#define BlockStride Size(2,2)	//block步长
+//#define CellSize Size(2,2)		//cell尺寸
+//#define Nbins 9					//直方图bin个数
+
+////HOG描述子参数
+//#define WinSize Size(64,32)		//检测窗口尺寸
+//#define BlockSize Size(8,8)		//block尺寸
+//#define BlockStride Size(4,4)		//block步长
+//#define CellSize Size(4,4)		//cell尺寸
+//#define Nbins 9					//直方图bin个数
+
+//detectMultiScale部分参数
+#define HitThreshold 0			//特征向量与超平面最小距离
+#define WinStride Size(8,8)		//移动步长(必须是block步长的整数倍)
+#define DetScale 1.1			//源图像图像每次缩小比例
+#define DetectResizeRate 1.4	//boxes' resizerate of detect's output
+
+#define TestImage "../Data/TestImage/13.jpg"				//用于检测的测试图像
+#define ResultImage "../Data/Result/13.jpg"					//测试图像的检测结果
+#define ResultImageFile_1 "..\\Data\\Result\\13-1\\"		//测试图像的分类框图文件夹1
+#define ResultImageFile_2 "..\\Data\\Result\\13-2\\"		//测试图像的分类框图文件夹2
+#define ResultImageFile_3 "..\\Data\\Result\\13-3\\"		//测试图像的分类框图文件夹3
+#define TestVideo "../Data/TestVideo/jade_s.avi"			//用于检测的测试视频
+#define ResultVideo "../Data/Result/jade_s.avi"			//测试视频的检测结果
+#define ResultVideoFile_1 "..\\Data\\Result\\out_1\\"	//测试视频的分类框图文件夹1
+#define ResultVideoFile_2 "..\\Data\\Result\\out_2\\"	//测试视频的分类框图文件夹2
+#define ResultVideoFile_3 "..\\Data\\Result\\out_3\\"	//测试视频的分类框图文件夹3
+
+#define IrobotSetFile "../Data/IrobotSet/"					//机器人样本图片文件夹
+#define ObstacleSetFile "../Data/ObstacleSet/"				//障碍样本图片文件夹
+#define BackgroundSetFile "../Data/BackgroundSet/"			//背景样本图片文件夹
+#define HardBackgroundSetFile "../Data/HardBackgroundSet/"	//Hard样本图片文件夹
+#define SetName "0SetName.txt"								//样本图片的文件名列表txt
+#define DetectSvmName "../Data/Result/SVM_HOG_Detect.xml"	//保存与读取的检测模型文件名称
+#define ClassifySvmName "../Data/Result/SVM_HOG_Classify.xml"//保存与读取的分类模型文件名称
+
+
+#define TrainPerc 1		//训练比例
+#define VaildPerc 0		//交叉验证比例
+//#define TestPerc 0.2
+#define IrobotTrainNo (int(IrobotSetNo * TrainPerc))//训练样本数量
+#define ObstacleTrainNo (int(ObstacleSetNo * TrainPerc))
+#define BackgroundTrainNo (int(BackgroundSetNo * TrainPerc))
+#define HardBackgroundTrainNo (int(HardBackgroundSetNo * TrainPerc))
+#define AllTrainNo (IrobotTrainNo + ObstacleTrainNo + BackgroundTrainNo + HardBackgroundTrainNo)
+#define IrobotVaildNo (int(IrobotSetNo * VaildPerc))//交叉验证样本数量
+#define ObstacleVaildNo (int(ObstacleSetNo * VaildPerc))
+#define BackgroundVaildNo (int(BackgroundSetNo * VaildPerc))
+#define HardBackgroundVaildNo (int(HardBackgroundSetNo * VaildPerc))
+#define AllVaildNo (IrobotVaildNo + ObstacleVaildNo + BackgroundVaildNo + HardBackgroundVaildNo)
+#define IrobotTestNo (IrobotSetNo - IrobotTrainNo - IrobotVaildNo)//测试样本数量
+#define ObstacleTestNo (ObstacleSetNo - ObstacleTrainNo - ObstacleVaildNo)
+#define BackgroundTestNo (BackgroundSetNo - BackgroundTrainNo - BackgroundVaildNo)
+#define HardBackgroundTestNo (HardBackgroundSetNo - HardBackgroundTrainNo - HardBackgroundVaildNo)
+#define AllTestNo (IrobotSetNo + ObstacleSetNo + BackgroundSetNo + HardBackgroundSetNo)
