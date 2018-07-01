fc1ffefdf4d6986e02f600c81c7539636afef396
kevin0525 <759424614@qq.com>
2018-07-01 17:01:06 +0800

    add data
    add data
    add a programme to test creating a directory

---
+++ svm_hog/svm_hog/test.cpp
@@ -0,0 +1,19 @@
+//#include <iostream>  
+//#include <fstream>  
+//#include <strstream>
+//#include <opencv2/core/core.hpp>  
+//#include <opencv2/highgui/highgui.hpp>  
+//#include <opencv2/imgproc/imgproc.hpp>  
+//#include <opencv2/objdetect/objdetect.hpp>  
+//#include <opencv2/ml/ml.hpp>  
+//#include "someMethod.h"
+//#include "parameter.h"
+//
+//using namespace std;  
+//using namespace cv;  
+//int main()
+//{
+//	const char * sd1 = {"md " ResultVideoFile_1};//创建存放检测框图的文件夹
+//	system(sd1);
+//	return 0;
+//}
